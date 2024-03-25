35c8552486fa0d1ae7840e8e2035d57013120d68
shiny <sws092298@outlook.com>
2024-03-25 03:49:47 +0200

    update the swagger description

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const document = SwaggerModule.createDocument(
     app,
     new DocumentBuilder()
-      .setTitle('MMA API')
-      .setDescription('MMA-HomeTask-API')
+      .setTitle('Company-Station API')
+      .setDescription(
+        'This is the electric vechile charging station management sytem api.',
+      )
       .setVersion('1.0')
       .build(),
   );
