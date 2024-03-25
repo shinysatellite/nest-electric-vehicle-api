18ce46254f93f31ca8be0630c4b3516a8fdc8a51
shiny <sws092298@outlook.com>
2024-03-25 03:56:35 +0200

    add swagger properties for companyDTO

--- src/company/dto/company.dto.ts
+++ src/company/dto/company.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CompanyDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   name: string;
 
   @IsNotEmpty()
   @IsNumber()
+  @ApiProperty()
   parent_company_id: number;
 }
